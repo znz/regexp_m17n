6b5ea2c1836d3ad67ecf7e379ea0725110e9a01b
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-08-02 03:29:42 +0900

    Fix spec for dummy encodings
    see https://github.com/os97673/regexp_m17n/issues/7

--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,6 +5,7 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
+      next if enc.dummy?
       assert(RegexpM17N.non_empty?('.'.encode(enc)))
     end
   end
