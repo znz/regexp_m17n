4d6ebda1067390735f746972351587d55e0ea6aa
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2014-08-02 04:01:42 +0900

    add String#b for ruby 1.9.3

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,3 +1,11 @@
+unless String.method_defined?(:b)
+  class String
+    def b
+      self.dup.force_encoding(Encoding::ASCII_8BIT)
+    end
+  end
+end
+
 module RegexpM17N
   def self.non_empty?(str)
     str.b =~ /^.+$/n
